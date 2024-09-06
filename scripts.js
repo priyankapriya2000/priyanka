51eb33419aa8db119cf37ef9214b2a115eb95288
priyanka <priyankapriya57037@gmail.com>
2024-09-06 08:53:28 +0530

    New animations done

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
-// Add JavaScript code here if needed
+// JavaScript Code
 console.log("JavaScript is working!");
 
-// Example: Add smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+    });
+});
+
+// Implement touch gestures for better mobile experience
+var projects = document.querySelectorAll('.project');
+
+projects.forEach(project => {
+    var hammer = new Hammer(project);
+    hammer.on('swipeleft', function() {
+        project.style.transform = 'translateX(-100px)';
+    });
+    hammer.on('swiperight', function() {
+        project.style.transform = 'translateX(100px)';
     });
 });
